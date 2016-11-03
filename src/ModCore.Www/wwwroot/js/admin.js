d5ca68cd23a7fbf4a98241a7fe4bf3edf927c7b7
Andrew Fry <andrew.fry+git@gmail.com>
2016-11-03 19:55:19 -0400

    Additional GUI commits for desktop. Mobile not completed.

--- src/ModCore.Www/wwwroot/js/admin.js
+++ src/ModCore.Www/wwwroot/js/admin.js
@@ -24,20 +24,37 @@
 //}
 
 function RegisterMenuEvents() {
-    $(".menu-extend-icon").click(function (e) {
-        if ($(".sidebar-main").hasClass("extended") || $(".body-wrapper").hasClass("extended")) {
-            $(".sidebar-main").removeClass("extended");
-            $(".body-wrapper").removeClass("extended");
+    //$(".menu-extend-icon").click(function (e) {
+    //    if ($(".sidebar-main").hasClass("extended") || $(".body-wrapper").hasClass("extended")) {
+    //        $(".sidebar-main").removeClass("extended");
+    //        $(".body-wrapper").removeClass("extended");
+    //    }
+    //    else {
+    //        $(".sidebar-main").addClass("extended");
+    //        $(".body-wrapper").addClass("extended");
+    //    }
+    //})
+    //$("#btnUserNavMenu").click(function (e) {
+    //    e.preventDefault();
+    //    $(".user-nav").toggleClass("extended");
+    //    $(this).toggleClass("extended");
+    //})
+
+    $(".menu-item ").click(function () {
+        if ($(this).hasClass("has-sub") == false) {
+            return;
+        }
+
+        if ($(this).hasClass("open")) {
+
+            $(this).removeClass("open");
+            $(this).children(".menu-group").slideUp(175);
         }
         else {
-            $(".sidebar-main").addClass("extended");
-            $(".body-wrapper").addClass("extended");
+            $(this).addClass("open");
+            $(this).children(".menu-group").slideDown(175);
         }
-    })
-    $("#btnUserNavMenu").click(function (e) {
-        e.preventDefault();
-        $(".user-nav").toggleClass("extended");
-        $(this).toggleClass("extended");
-    })
 
+    });
 }
+
