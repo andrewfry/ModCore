9660818848fa12756b8b11ee3564c05edee4e64d
Andrew Fry <andrew.fry+git@gmail.com>
2016-12-08 06:01:03 -0500

    Updated the plugin publish command to only copy view folders if they exist as it throws an error otherwise.

--- src/cmds/plugin-publish.cmd
+++ src/cmds/plugin-publish.cmd
@@ -13,9 +13,16 @@ cd ..
 @echo CURRENT DIR 
 cd
 @echo COMMAND TO RUN
+
+REM Copy all of the assemblies to the plugin folder in the ModCore.www project
 @echo xcopy "plugin-src\%1%binLoc%" "%pluginLoc%%2%" /c /i /s /e /y
 xcopy  /c /i /s /e /y /v  "plugin-src\%1%binLoc%" "%pluginLoc%%2%"
 
+REM If there is a Views folder in the plugin then copy it as well
+IF EXIST "plugin-src\%1%ViewLoc%" (
 @echo xcopy "plugin-src\%1%ViewLoc%" "%pluginLoc%%2%ViewLoc%" /c /i /s /e /y
 xcopy  /c /i /s /e /y /v  "plugin-src\%1%ViewLoc%" "%pluginLoc%%2%\Views"
+)
+
+REM Delete any ModCore specific assembilies as they will be rejected by the plugin manager and do not need to be copied.
 del "%pluginLoc%%2%\ModCore*.*"
---
+++ src/plugin-src/BasicAuthentication.Plugin/Filter/AdminAuthFilter2.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ModCore.Core.Controllers;
+using System.Threading.Tasks;
+using ModCore.Abstraction.Site;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BasicAuthentication.Plugin.Filters
+{
+    public class AdminAuthFilter2 : IAsyncAuthorizationFilter, IFilterMetadata
+    {
+        private readonly ISiteSettingsManagerAsync _siteSettings;
+
+        public AdminAuthFilter2(ISiteSettingsManagerAsync siteSettings, AuthorizationPolicy policy)
+        {
+            _siteSettings = siteSettings;
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            //if (context.RouteData.Values["Area"] == null)
+            //{
+            //    return Task.FromResult(0);
+            //}
+
+            //var isAdminRoute = string.Compare(context.RouteData.Values["Area"].ToString(), "Admin", true) == 0;
+            //if (!isAdminRoute)
+            //{
+            //    return Task.FromResult(0);
+            //}
+
+            //if (context.RouteData.Values["Controller"].ToString().ToLower() == "account" && context.RouteData.Values["Action"].ToString().ToLower() == "login")
+            //{
+            //    return Task.FromResult(0);
+            //}
+
+            //var controller = context. as BaseController;
+
+            //if (controller.CurrentSession.IsLoggedIn)
+            //{
+            //    var authService = context.HttpContext.RequestServices.GetService(typeof(IAuthenticationService)) as IAuthenticationService;
+            //    var userId = controller.CurrentSession.UserId;
+
+
+            //    var routeData = context.RouteData;
+            //    //var isAllowed = await authService.UserAllowedAdminAccess(userId, routeData);
+            //    var isAllowed = true;
+
+            //    if (!isAllowed)
+            //    {
+            //        context.Result = controller.RedirectToAction("Error", new { id = "404" });
+            //    }
+
+            //    await next();
+            //    return;
+            //}
+
+            //context.HttpContext.Response.Redirect($"/Admin/Account/Login?ReturnUrl={context.HttpContext.Request.Path}");
+        }
+
+    }
+}
