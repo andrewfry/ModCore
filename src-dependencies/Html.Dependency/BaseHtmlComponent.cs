97d7493ee2918b3b4009c0e731f107637a36b14d
Matt Crain <mattcrain87@gmail.com>
2017-04-13 08:45:43 -0400

    Html Dpc-src added

---
+++ src-dependencies/Html.Dependency/BaseHtmlComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModHtml.Dependency
+{
+    public class BaseHtmlComponent : IHtmlComponent
+    {
+        public string Name { get; set; }
+
+        public string BaseClass { get; set; }
+
+        public int Position { get; set; }
+
+        public string DisplayTypeName { get; set; } 
+
+    }
+}
---
+++ src-dependencies/Html.Dependency/IHtmlComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModHtml.Dependency
+{
+    public interface IHtmlComponent
+    {
+        string Name { get;  }
+
+        string BaseClass { get; }
+
+        int Position { get; }
+
+        string DisplayTypeName { get;}
+    }
+}
